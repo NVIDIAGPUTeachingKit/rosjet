bea20f90e202cfe00f6239f6dcc070ac2d3779b7
jkerfs <jkerfs@calpoly.edu>
2016-05-06 13:55:41 -0700

    reorganized packages, added urdf

--- data_recorder/src/data_recorder.cpp
+++ data_recorder/src/data_recorder.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/videoio.hpp"
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/Image.h>
 #include <std_msgs/Empty.h>
